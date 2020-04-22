e1f89c6dbbe4facf35a930531727dd8f56910262
wutao <yuycc@foxmail.com>
2020-04-22 18:10:06 +0800

    update weapp

--- WxWeApp.php
+++ WxWeApp.php
@@ -13,10 +13,11 @@ class WxWeApp
 
 	private function __clone(){}
 
-	private function __construct(string $appId, string $appSecret)
+	private function __construct(string $appId, string $appSecret, string $mchId = null)
 	{
 		$this->wxConfig['app_id'] = $appId;
 		$this->wxConfig['app_secret'] = $appSecret;
+		$this->wxConfig['mch_id'] = $mchId;
 	}
 
 	/**
@@ -62,8 +63,46 @@ class WxWeApp
 		}
 	}
 
-	public function FunctionName($value='')
+	/**
+	 * 获取小程序全局唯一后台接口调用凭据access_token,调用大多数后台接口都需access_token
+	 * @return [array]
+	 */
+	public function getAccessToken()
 	{
-		# code...
+		try {
+			require 'Curl.php';
+			$curl = Curl::instance();
+			$result = $curl->get($this->wxApiUrl . '/cgi-bin/token', [
+				'grant_type'=> 'client_credential',
+				'appid'=> $this->wxConfig['app_id'],
+				'secret'=> $this->wxConfig['app_secret']
+			]);
+			return json_decode($result, true);
+		} catch (\Exception $e) {
+			exit($e->getMessage());
+		}
+	}
+
+	/**
+	 * 用户支付完成后，获取该用户的UnionId，无需用户授权
+	 * 注意：调用前需要用户完成支付，且在支付后的五分钟内有效
+	 * @param string $value [description]
+	 */
+	public function FunctionName(string $accessToken, string $openId, string $transactionId = '', string $outTradeNo = '')
+	{
+		if (empty($accessToken)) {
+			exit('请传入access_token！');
+		}
+		try {
+			require 'Curl.php';
+			$curl = Curl::instance();
+			$result = $curl->get($this->wxApiUrl . '/wxa/getpaidunionid', [
+				'access_token' => trim($accessToken),
+				'openid' => $openId
+			]);
+			return json_decode($result, true);
+		} catch (\Exception $e) {
+			exit($e->getMessage());
+		}
 	}
 }
